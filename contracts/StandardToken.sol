0ed1f675c3829556e658b52a2103708bb02ef874
Andrew B. Collier <andrew@exegetic.biz>
2017-12-18 14:22:56 +0200

    Renamed constructor.

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -9,7 +9,7 @@ contract StandardToken is Token {
     uint8 public decimals;
     string public symbol;
 
-     function HumanStandardToken(uint256 _initialAmount, string _tokenName, uint8 _decimalUnits, string _tokenSymbol) public {
+     function StandardToken(uint256 _initialAmount, string _tokenName, uint8 _decimalUnits, string _tokenSymbol) public {
         balances[msg.sender] = _initialAmount;               // Give the creator all initial tokens
         totalSupply = _initialAmount;                        // Update total supply
         name = _tokenName;                                   // Set the name for display purposes
