94a111e6e396a9e6c36c4d65143c2c1f2142c29a
Andrew B. Collier <andrew@exegetic.biz>
2017-12-20 05:40:49 +0200

    Test balance of owner account.

--- test/standard_token.js
+++ test/standard_token.js
@@ -1,13 +1,22 @@
 var StandardToken = artifacts.require("StandardToken.sol");
 
 contract('StandardToken', function(accounts) {
-  it("initial supply of tokens", function() {
-    var standard_token;
-    return StandardToken.deployed().then(function(instance){
-     standard_token = instance;
-     return standard_token.totalSupply.call();
-    }).then(function(result){
-     assert.equal(result.toNumber(), 10000, 'total supply is wrong');
-    })
-  });
+	it("initial supply of tokens", function() {
+		var standard_token;
+		return StandardToken.deployed().then(function(instance) {
+			standard_token = instance;
+			return standard_token.totalSupply.call();
+		}).then(function(result){
+			assert.equal(result.toNumber(), 10000, 'total supply is wrong');
+		})
+	});
+	it("balance of token owner", function() {
+		var standard_token;
+		return StandardToken.deployed().then(function(instance){
+			standard_token = instance;
+			return standard_token.balanceOf(web3.eth.accounts[0]);
+		}).then(function(result){
+			assert.equal(result.toNumber(), 10000, 'balance is wrong');
+		})
+	});
 });
