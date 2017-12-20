e3818e7d6ea1f94ca00efbf1ecb36f139ae0db51
Andrew B. Collier <andrew@exegetic.biz>
2017-12-20 06:03:10 +0200

    Consolidated contract code.

--- contracts/StandardToken.sol
+++ contracts/StandardToken.sol
@@ -1,20 +1,29 @@
 pragma solidity ^0.4.18;
 
-import "./Token.sol";
-
-contract StandardToken is Token {
+contract StandardToken {
     uint256 constant MAX_UINT256 = 2**256 - 1;
 
+    uint256 public totalSupply;                                 // Total number of tokens.
     string public name;
     string public symbol;
 
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
      function StandardToken(uint256 _initialAmount, string _tokenName, string _tokenSymbol) public {
-        balances[msg.sender] = _initialAmount;               // All initial tokens allocated to creator
+        balances[msg.sender] = _initialAmount;                  // All initial tokens allocated to creator
         totalSupply = _initialAmount;
         name = _tokenName;
         symbol = _tokenSymbol;
     }
 
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
     function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balances[msg.sender] >= _value);
         balances[msg.sender] -= _value;
@@ -23,6 +32,11 @@ contract StandardToken is Token {
         return true;
     }
 
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return Whether the transfer was successful or not
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         uint256 allowance = allowed[_from][msg.sender];
         require(balances[_from] >= _value && allowance >= _value);
@@ -35,21 +49,26 @@ contract StandardToken is Token {
         return true;
     }
 
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
     function balanceOf(address _owner) view public returns (uint256 balance) {
         return balances[_owner];
     }
 
+    /// @notice `msg.sender` approves `_spender` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of tokens to be approved for transfer
+    /// @return Whether the approval was successful or not
     function approve(address _spender, uint256 _value) public returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);                 // Trigger Approval event
         return true;
     }
 
-    function allowance(address _owner, address _spender)
-    view public returns (uint256 remaining) {
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return Amount of remaining tokens allowed to spent
+    function allowance(address _owner, address _spender) view public returns (uint256 remaining) {
       return allowed[_owner][_spender];
     }
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
 }
